90c0974788806cc54b170b597a23c0255c5d9549
Nico F. <50293660+nicofisseler@users.noreply.github.com>
2025-01-14 15:59:11 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+// service-worker.js
+const CACHE_NAME = "timer-cache-v1";
+const FILES_TO_CACHE = ["/", "/index.html", "/favicon.ico"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
